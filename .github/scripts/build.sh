6d7b21969f3fa8e1faf5854e33db389d876b13b8
Evgeny Karpov <evgeny@kmaps.co>
2024-04-04 20:07:51 +0200

    Add mod-openvino to the build

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,6 +4,10 @@ source `dirname ${BASH_SOURCE[0]}`/config.sh
 
 mkdir -p $BUILD_PATH
 cd $BUILD_PATH
+
+cp -r mod-openvino $SOURCE_PATH/$AUDACITY_VERSION/modules
+sed -i '' 's/set( MODULES/set( MODULES\n   mod-openvino/' $SOURCE_PATH/$AUDACITY_VERSION/modules/CMakeLists.txt
+
 cmake -G "Unix Makefiles" $SOURCE_PATH/$AUDACITY_VERSION -DCMAKE_BUILD_TYPE=Release
 make -j`sysctl -n hw.ncpu`
 
