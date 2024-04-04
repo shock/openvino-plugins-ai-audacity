466a6e7c7c1481893702de02ab1999a75268dc42
Evgeny Karpov <evgeny@kmaps.co>
2024-04-04 20:15:43 +0200

    Add WORKSPACE_PATH to fix the build script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,7 +5,7 @@ source `dirname ${BASH_SOURCE[0]}`/config.sh
 mkdir -p $BUILD_PATH
 cd $BUILD_PATH
 
-cp -r mod-openvino $SOURCE_PATH/$AUDACITY_VERSION/modules
+cp -r $WORKSPACE_PATH/mod-openvino $SOURCE_PATH/$AUDACITY_VERSION/modules
 sed -i '' 's/set( MODULES/set( MODULES\n   mod-openvino/' $SOURCE_PATH/$AUDACITY_VERSION/modules/CMakeLists.txt
 
 cmake -G "Unix Makefiles" $SOURCE_PATH/$AUDACITY_VERSION -DCMAKE_BUILD_TYPE=Release
--- .github/scripts/config.sh
+++ .github/scripts/config.sh
@@ -5,3 +5,4 @@ set -x # echo on
 set -o pipefail # fail of any command in pipeline is an error
 
 BUILD_PATH=${BUILD_PATH:-$PWD/build}
+WORKSPACE_PATH=$(pwd)
