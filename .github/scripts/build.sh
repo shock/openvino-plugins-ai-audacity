c12898224b8d3908bed9cb79dc25332fae040af9
Evgeny Karpov <evgeny@kmaps.co>
2024-04-04 20:29:52 +0200

    Create PACKAGE_PATH folder

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,6 +5,8 @@ source `dirname ${BASH_SOURCE[0]}`/config.sh
 cp -r $WORKSPACE_PATH/mod-openvino $SOURCE_PATH/$AUDACITY_VERSION/modules
 sed -i '' 's/set( MODULES/set( MODULES\n   mod-openvino/' $SOURCE_PATH/$AUDACITY_VERSION/modules/CMakeLists.txt
 
+mkdir -p $PACKAGE_PATH
+
 cd $PACKAGE_PATH
 wget https://storage.openvinotoolkit.org/repositories/openvino/packages/2024.0/macos/m_openvino_toolkit_macos_11_0_2024.0.0.14509.34caeefd078_arm64.tgz
 tar xvf m_openvino_toolkit_macos_11_0_2024.0.0.14509.34caeefd078_arm64.tgz
